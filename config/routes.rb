8c7c09cffde4f424744f80dd071108aa23bb0183
sylvain <plaisir@odevie.org>
2015-11-18 10:59:13 +0100

    tradz + callback route in post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Spree::Core::Engine.routes.draw do
   scope module: 'payment_methods' do
     get '/cyber_plus_paiement/:gateway_id/:order_id', controller: 'cyber_plus_paiement', action: 'show', as: :gateway_cyber_plus_paiement
-    get '/cyber_plus_paiement/callback', controller: 'cyber_plus_paiement', action: 'callback'
+    post '/cyber_plus_paiement/callback', controller: 'cyber_plus_paiement', action: 'callback'
     get '/cyber_plus_paiement/comeback', controller: 'cyber_plus_paiement', action: 'comeback'
   end
 end
